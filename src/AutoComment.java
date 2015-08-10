832c3d551955fe1075485ec4e3347062a3d30563
jpdurot <jeanphilippe.durot@gmail.com>
2015-08-10 18:37:26 +0200

    Not working check-in

--- src/AutoComment.java
+++ src/AutoComment.java
@@ -1,10 +1,16 @@
+import com.intellij.lang.Language;
+import com.intellij.lang.PsiBuilderFactory;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.fileTypes.LanguageFileType;
 import com.intellij.openapi.ui.Messages;
+import com.intellij.psi.PsiComment;
 import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiParserFacade;
 import com.intellij.psi.impl.source.tree.LeafPsiElement;
 import com.intellij.psi.util.PsiTreeUtil;
+import com.jetbrains.cidr.lang.OCLanguage;
 import com.jetbrains.cidr.lang.parser.OCTokenTypes;
 import com.jetbrains.cidr.lang.psi.OCDeclarator;
 import com.jetbrains.cidr.lang.psi.OCFunctionDefinition;
@@ -13,6 +19,7 @@ import com.jetbrains.cidr.lang.psi.OCParameterList;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
 
 /**
  * Created by jp on 08/08/15.
@@ -25,7 +32,13 @@ public class AutoComment extends AnAction {
             OCDeclarator declaration = PsiTreeUtil.getChildOfType(function, OCDeclarator.class);
             OCParameterList parameterList = PsiTreeUtil.getChildOfType(declaration, OCParameterList.class);
             String methodName = getIdentifierValue(declaration);
-            Messages.showMessageDialog(methodName, "Auto comment (method)", null);
+            PsiParserFacade factory = PsiParserFacade.SERVICE.getInstance(e.getData(PlatformDataKeys.PROJECT));
+            LanguageFileType language = (LanguageFileType)e.getData(PlatformDataKeys.PSI_FILE).getFileType();
+            String commentToAdd = String.format(Locale.FRENCH,"%s description", methodName);
+            PsiComment comment =factory.createLineCommentFromText(language, commentToAdd);
+            //Messages.showMessageDialog(methodName, "Auto comment (method)", null);
+            PsiElement parent = declaration.getParent();
+            parent.addBefore(comment, declaration);
             List<OCParameterDeclaration> parameters = PsiTreeUtil.getChildrenOfTypeAsList(parameterList, OCParameterDeclaration.class);
             for (int i=0;i<parameters.size();i++)
             {
